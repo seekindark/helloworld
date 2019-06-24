d27408fbda01b195829c14ab444da928f2a9f0f2
seekindark <fei_n_chen@163.com>
2019-06-24 21:14:46 +0800

    add a words at the end

--- hw.c
+++ hw.c
@@ -3,5 +3,5 @@
 int main()
 {
   printf ("Hello Shanghai, Bejing!--missing an enterring\n");
-  printf("insert a new line to test git\n");
+  printf("insert a new line to test git frequently-used commands\n");
 }
