3e72ad4c183aa1d727510f3651bd056d67ae9075
seekindark <fei_n_chen@163.com>
2020-04-08 18:56:23 +0800

    Update hw.c
    add a testline

--- hw.c
+++ hw.c
@@ -5,5 +5,5 @@ int main()
   printf ("Hello Shanghai, Bejing!--missing an enterring\n");
   printf("insert a new line to test git frequently-used commands\n");
   printf("insert a new line to test git\n");
-print(" new feature from branch hw.c"
+  print(" new feature from branch hw.c\n");  //cf: added for test
 }
